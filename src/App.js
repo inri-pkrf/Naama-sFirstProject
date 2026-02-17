289a53a95f3104b8c514e6e5148e6378ffc4f061
CherryTomato21 <noa.shperber@gmail.com>
2026-02-17 10:12:21 +0200

    1

--- src/App.js
+++ src/App.js
@@ -7,33 +7,40 @@ import GueseWho from './componentsJsx/gueseWho.jsx';
 import PuzzleReveal from './componentsJsx/puzzleReveal.jsx';
 import GamesIntro from './componentsJsx/gamesIntro.jsx';
 import './App.css';
+
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   const [placeIndex, setPlaceIndex] = useState(0);
   const [placeGlow, setPlaceGlow] = useState(0);
 
   const resetMap = () => {
-  setPlaceIndex(0);
-  setPlaceGlow(0);
-};
-
+    setPlaceIndex(0);
+    setPlaceGlow(0);
+  };
 
   return (
-    <Router>
+    <HashRouter>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/map' element={<Map placeIndex={placeIndex} setPlaceIndex={setPlaceIndex} placeGlow={placeGlow} setPlaceGlow={setPlaceGlow}/>}/>
-        <Route path='/place/:id' element={<PlaceInfo/>}/>
-        <Route path='/gamesIntro' element={<GamesIntro/>}/>
-        <Route path='/game1' element={<GameTwo/>}/>
-        <Route path='/game2' element={<GueseWho/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/map' element={
+          <Map
+            placeIndex={placeIndex}
+            setPlaceIndex={setPlaceIndex}
+            placeGlow={placeGlow}
+            setPlaceGlow={setPlaceGlow}
+          />
+        } />
+        <Route path='/place/:id' element={<PlaceInfo />} />
+        <Route path='/gamesIntro' element={<GamesIntro />} />
+        <Route path='/game1' element={<GameTwo />} />
+        <Route path='/game2' element={<GueseWho />} />
         <Route path="/game3" element={<PuzzleReveal />} />
-        <Route path='/summary' element={<Summary resetMap={resetMap}/>}/>
-        <Route path='/home' element={<Home/>}/>
+        <Route path='/summary' element={<Summary resetMap={resetMap} />} />
+        <Route path='/home' element={<Home />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 }
 
